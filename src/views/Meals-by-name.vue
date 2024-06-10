f5c7f1364ab2310376a49f31926b7c124f8854ff
lhadi <lhadidib07@gmail.com>
2024-06-10 11:19:38 +0100

    added mealItem

--- src/views/Meals-by-name.vue
+++ src/views/Meals-by-name.vue
@@ -7,38 +7,23 @@
       placeholder="Search for meals by Name "
       @change="searchMeals" />
   </div>
-  <div v-if="meals.length > 0" class="grid grid-cols-1 md:grid-cols-3 gap-5 px-8">
-    <div
-      v-for="meal in meals"
-      :key="meal.idMeal"
-      class="bg-white shadow rounded-xl hover:scale-105 transition-all">
-      <router-link :to="{name:'mealDetails' ,params:{id:meal.idMeal }}">
-      <img
-        :src="meal.strMealThumb"
-        :alt="meal.strMeal"
-        class="rounded-t-xl w-full h-48 object-cover" />
-      </router-link>
-      <div class="p-3">
-        <h3 class="font-bold">{{ meal.strMeal }}</h3>
-
-        <div class="flex items-center justify-between">
-         <YoutubeButton :href="meal.strYoutube">Youtube</YoutubeButton>
-        </div>
-      </div>
-    </div>
+  <div
+    v-if="meals.length > 0"
+    class="grid grid-cols-1 md:grid-cols-3 gap-5 px-8">
+    <MealItem v-for="meal in meals" :key="meal.idMeal" :meal=meal />
   </div>
-  
+  <div v-else>no meal found</div>
 </template>
 
 <script setup>
-import YoutubeButton from "../components/YoutubeButton.vue";
+import MealItem from "../components/MealItem.vue";
+
 import { computed } from "vue";
-import { onMounted,ref } from "vue";
+import { onMounted, ref } from "vue";
 import { useRoute } from "vue-router";
 import store from "@/store";
 
-
-const route = useRoute(); 
+const route = useRoute();
 const keyword = ref("");
 const meals = computed(() => store.state.searchedMeals);
 
@@ -46,10 +31,10 @@ function searchMeals() {
   store.dispatch("searchMeals", keyword.value);
 }
 
-onMounted(() => { 
-    keyword.value = route.params.name; 
-    if(keyword.value){ 
-      searchMeals()
-    }
-})
+onMounted(() => {
+  keyword.value = route.params.name;
+  if (keyword.value) {
+    searchMeals();
+  }
+});
 </script>
